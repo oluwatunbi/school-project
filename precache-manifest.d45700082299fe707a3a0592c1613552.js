d651de8b665d59e11e64f629e4550a6217513cee
oluwatunbi <davies899@yahoo.com>
2023-09-09 22:14:38 +0100

    Updates

---
+++ precache-manifest.d45700082299fe707a3a0592c1613552.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "36587cad78c0d1858c9c",
+    "url": "/static/css/main.99b53a6d.chunk.css"
+  },
+  {
+    "revision": "36587cad78c0d1858c9c",
+    "url": "/static/js/main.36587cad.chunk.js"
+  },
+  {
+    "revision": "9eb496fb0fe0fa5c7bf1",
+    "url": "/static/js/1.9eb496fb.chunk.js"
+  },
+  {
+    "revision": "ae70ac8b6e9a19d4067b",
+    "url": "/static/js/2.ae70ac8b.chunk.js"
+  },
+  {
+    "revision": "366d15cf6f49138d0742",
+    "url": "/static/js/runtime~main.366d15cf.js"
+  },
+  {
+    "revision": "446bdc422b36efab1e05894faa59fed6",
+    "url": "/static/media/Onias-pic.446bdc42.jpg"
+  },
+  {
+    "revision": "17893152bf4677d91327b95abb8980c2",
+    "url": "/static/media/school-pic.17893152.jpg"
+  },
+  {
+    "revision": "ccadbc5076232c2235cfe31e313bc0f4",
+    "url": "/static/media/school.ccadbc50.jpg"
+  },
+  {
+    "revision": "aef7109bf37890a6f84a4bfb59457ed4",
+    "url": "/static/media/teacher.aef7109b.jpg"
+  },
+  {
+    "revision": "b351a08a2d6b9bae8a67423375e5eb07",
+    "url": "/static/media/kids.b351a08a.jpg"
+  },
+  {
+    "revision": "0080307dca3f7184a60450e88f2bdb8f",
+    "url": "/static/media/classpupil.0080307d.jpg"
+  },
+  {
+    "revision": "0a09d952b2e82fec8a6eae701d9d889b",
+    "url": "/index.html"
+  }
+];
